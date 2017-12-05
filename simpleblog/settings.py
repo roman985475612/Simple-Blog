d212c0695ea78475b97e71cb312f2339c5e0d651
Roman <roma2@protonmail.com>
2017-12-05 15:39:17 +0300

    Change settings.py

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -145,8 +145,10 @@ if os.getcwd() == '/app':
 
     ALLOWED_HOSTS = ['*']
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
-    STATICFILESDIRS = (
-        os.path.join(BASE_DIR, 'static'),
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
     )
