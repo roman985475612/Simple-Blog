a5029347175a8b9c3b9aaf5069b1e25f5d7bca42
Roman <roma2@protonmail.com>
2017-12-08 17:13:02 +0300

    Change settings.py

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -152,8 +152,9 @@ if os.getcwd() == '/app':
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+    # STATICFILES_DIRS = (
+    #     os.path.join(BASE_DIR, 'static'),
+    # )
