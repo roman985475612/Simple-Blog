a215dbb2c9896f536bdf9c2535a1c2553951c6d3
Roman <roma2@protonmail.com>
2017-12-13 16:01:06 +0300

    Task # 29 complete: added profile view page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('login/', auth_views.login, {'template_name': 'accounts/login.html'}, name='login'),
     path('logout/', auth_views.logout, {'next_page': '/'}, name='logout'),
     path('register/', views.RegisterFormView.as_view(), name='register'),
+    path('profile/', TemplateView.as_view(template_name='accounts/profile.html'), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import FormView
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -17,3 +19,5 @@ class RegisterFormView(FormView):
         user = authenticate(username=username, password=password)
         login(self.request, user)
         return super().form_valid(form)
+
+
